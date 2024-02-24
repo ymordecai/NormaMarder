e1a991b9acddd4b72aabfe454a668eb53297960a
ymordecai <yuri@yurimarder.com>
2024-02-24 17:11:03 -0500

    Initial commit

--- src/partials/hire/index.jsx
+++ src/partials/hire/index.jsx
@@ -24,7 +24,7 @@ const Hire = (props) => (
         <h1 className='_title'>Music</h1>
 
         <h2 className='_title'>
-                        <iframe width="80%" height="400" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1764075174&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+                        <iframe width="80%" height="400" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1764075174&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
         </h2>
 
       {/* Button part */}
