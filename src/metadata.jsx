57da89c7a153ff652804cb7f298ee08397f97f72
ymordecai <yuri@yurimarder.com>
2024-02-24 21:56:20 -0500

    Initial commit

--- src/metadata.jsx
+++ src/metadata.jsx
@@ -14,6 +14,12 @@ const Metadata = () => (
     <link rel='icon' sizes='32x32' href='/favicon.png' />
     <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
     <meta name='viewport' content='width=device-width, initial-scale=1' />
+
+    <meta property='og:title' content='In Memory of Norma Marder - 1934-2023' />
+<meta property='og:type' content='website' />
+<meta property='og:url' content='https://norma-marder.vercel.app/' />
+<meta property='og:image' content='https://norma-marder.vercel.app/partials/hero/background.jpg' />
+<meta property='og:description' content='A tribute writer and singer Norma Marder who lived an exquisite life and left a beautiful legacy.' />
   </Head>
 )
 
